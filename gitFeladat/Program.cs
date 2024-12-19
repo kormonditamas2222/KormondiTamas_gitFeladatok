99ca60674758688cd318c81659e2ef068fb1dcbb
kormonditamas2222 <t.kormond2006@gmail.com>
2024-12-19 16:43:59 +0100

    f01 feladat elkészítve

---
+++ gitFeladat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitFeladat
+{
+    internal class Program
+    {
+        static void f01(int[] tomb)
+        {
+            Random random = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = random.Next(1, 51);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
